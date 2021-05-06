b7322ac1c745488661f9077a5a55b3adf30d8f04
atemengue <moafembe@gmail.com>
2021-05-06 04:22:58 +0100

    typeScript for state

--- src/state/UserSearch.tsx
+++ src/state/UserSearch.tsx
@@ -6,15 +6,26 @@ const users = [
   { name: 'Micheal', age: 50 },
 ]
 
-
-
 const UserSearch: React.FC = () => {
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<{ name: String, age: number } | undefined >()
 
-  const onClick = () => { }
+  const onClick = () => { 
+    const foundUser = users.find(user => {
+      return user.name == name
+    })
+
+   setUser(foundUser)
+
+  }
+
   return <div>User Search
     <input value={name} onChange={e => setName(e.target.value)} />
     <button onClick={onClick} >Find User</button>
+    <div>
+      {user && user.name}
+      {user && user.age}
+    </div>
   </div>
 }
 
