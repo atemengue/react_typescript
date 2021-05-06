4227e57f610cb6baef7d0d35c79b5e953f3f8011
atemengue <moafembe@gmail.com>
2021-05-06 04:15:31 +0100

    continued with props

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom';
+import UserSearch from './state/UserSearch';
+
 
 const App = () => {
   return (
     <div>
-      <h1>Hi there</h1>
+     <UserSearch />
     </div>
   )
 }
